e7788bc9b9063de92ec9047ac29efb36bd367800
Battula-anil-app-could <124858298+Battula-anil-app-could@users.noreply.github.com>
2024-01-23 11:22:22 +0530

    0.1.0

--- dharitri-wasm/src/types/interaction/mod.rs
+++ dharitri-wasm/src/types/interaction/mod.rs
@@ -1,6 +1,7 @@
 mod arg_buffer;
 mod async_call;
 mod callback_call;
+mod callback_selector_result;
 mod contract_call;
 mod send_moax;
 mod send_dct;
@@ -9,6 +10,7 @@ mod send_token;
 pub use arg_buffer::ArgBuffer;
 pub use async_call::AsyncCall;
 pub use callback_call::CallbackCall;
+pub use callback_selector_result::CallbackSelectorResult;
 pub use contract_call::{new_contract_call, ContractCall};
 pub use send_moax::SendMoax;
 pub use send_dct::SendDct;
