08778d9bc747d0f07259059fb7fa8a73aae083b4
Battula-anil-app-could <124858298+Battula-anil-app-could@users.noreply.github.com>
2024-02-16 16:32:16 +0530

    0.11.4

--- framework/meta/src/sc_upgrade/upgrade_versions.rs
+++ framework/meta/src/sc_upgrade/upgrade_versions.rs
@@ -1,7 +1,7 @@
 /// Not necessarily the last entry in `VERSIONS`.
 ///
 /// Indicates where to stop with the upgrades.
-pub const DEFAULT_LAST_VERSION: &str = "0.11.3";
+pub const DEFAULT_LAST_VERSION: &str = "0.11.4";
 
 /// Known version for the upgrader.
 #[rustfmt::skip]
@@ -27,7 +27,8 @@ pub const VERSIONS: &[&str] = &[
     "0.39.1",
     "0.39.2",
     "0.39.3",
-    "0.11.3",
+    "0.39.4",
+    "0.11.4",
 ];
 
 pub struct VersionIterator {
