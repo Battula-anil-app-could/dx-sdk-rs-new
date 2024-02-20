945fe3c09af6d0bd2e8e8dff1b6c0a5ec2b58908
Battula-anil-app-could <124858298+Battula-anil-app-could@users.noreply.github.com>
2024-02-20 19:45:29 +0530

    0.12.5

--- framework/meta/src/cmd/standalone/upgrade/upgrade_versions.rs
+++ framework/meta/src/cmd/standalone/upgrade/upgrade_versions.rs
@@ -39,9 +39,8 @@ pub const VERSIONS: &[&str] = &[
      "0.12.2",
      "0.12.3",
      "0.12.4",
-     "0.12.5",
- ];
 
+ ];
 pub struct VersionIterator {
     next_version: usize,
     last_version: String,
